47e62a47100ed18f876a009f0f134778a158c6f4
nitesh <nitesh@fusionofideas.com>
2021-06-17 16:45:29 -0700

    Initial Commit

---
+++ SwiftUITut/SwiftUITutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutApp.swift
+//  SwiftUITut
+//
+//  Created by Nitesh Mainaly on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
